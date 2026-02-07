f8bfde8081c8cc3ce907fa105aff7d6d3c2cf5eb
BalaBenna <balakrishnabenna3204@gmail.com>
2026-02-07 13:55:51 +0530

    fiixed header

--- app/src/components/layout/Header.tsx
+++ app/src/components/layout/Header.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { motion } from 'framer-motion'
 import {
-  ChevronDown,
   User,
   Settings,
   LogOut,
@@ -12,6 +11,7 @@ import {
   Folder,
   Crown,
   ImageIcon,
+  ChevronDown,
 } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
